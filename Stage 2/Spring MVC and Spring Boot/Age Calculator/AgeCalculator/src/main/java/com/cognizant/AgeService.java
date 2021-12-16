8d5dac71421788754334ef4ec2c7b418d9e3048f
ADM21JF008009 <92528233+ADM21JF-008009@users.noreply.github.com>
2021-12-17 01:08:54 +0530

    Update AgeService.java

--- Stage 2/Spring MVC and Spring Boot/Age Calculator/AgeCalculator/src/main/java/com/cognizant/AgeService.java
+++ Stage 2/Spring MVC and Spring Boot/Age Calculator/AgeCalculator/src/main/java/com/cognizant/AgeService.java
@@ -10,16 +10,12 @@ public class AgeService {
 
 		// add the code as per the requirement
 
-		// Current date
 		LocalDate today = LocalDate.now();
 
-		// Parsed dob from client
 		LocalDate dob = LocalDate.parse(date, DateTimeFormatter.ofPattern("yyyy-MM-dd"));
 
-		// Calculating the age by finding time period between today and dob
 		Period age = Period.between(dob, today);
 
-		// Returning the required output format
 		return "you are " + age.getYears() + " years, " + age.getMonths() + " months, " + age.getDays() + " days old.";
 	}
 }
